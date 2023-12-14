1cb8e87cb0b430b1e6efbae26fce88d52ca0d0e2
Alex <sakradnes@mail.ru>
2023-12-14 18:05:44 +0300

    first commit

---
+++ server/app.js
@@ -0,0 +1,21 @@
+require('@babel/register');
+require('dotenv').config();
+
+const express = require('express');
+
+const indexRouter = require('./routes/index.route');
+const serverConfig = require('./config/serverConfig');
+const checkUser = require('./middlewares/verifyJWT');
+
+const app = express();
+
+serverConfig(app);
+app.use(checkUser);
+
+app.use('/', indexRouter);
+
+const PORT = process.env.PORT ?? 3000;
+
+app.listen(PORT, () => {
+  console.log('Сервер');
+});
---
+++ server/config/serverConfig.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const ssr = require('../middlewares/ssr');
+const checkUser = require('../middlewares/verifyJWT');
+
+const config = (app) => {
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.static('public'));
+  app.use(ssr);
+  app.use(cookieParser());
+  app.use(checkUser);
+};
+
+module.exports = config;

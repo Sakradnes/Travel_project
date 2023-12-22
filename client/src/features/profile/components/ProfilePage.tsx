03a7611c978bb40ba02402a96701daa4ef6b70eb
Alex <sakradnes@mail.ru>
2023-12-22 11:39:10 +0300

    css2 Profile and Main

--- client/src/features/profile/components/ProfilePage.tsx
+++ client/src/features/profile/components/ProfilePage.tsx
@@ -1,14 +1,15 @@
 import React, { useState } from 'react';
 import Modal from 'react-modal';
-import { useSelector } from 'react-redux';
 import Slider from 'react-slick';
+import { useSelector } from 'react-redux';
 import { useAppDispatch, type RootState } from '../../../store/store';
 import AddPhoto from './AddPhoto';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
 import { deletePhotoAlbum } from '../../auth/authSlice';
 import btnDel from '../../../../public/icons8-отмена.svg';
 import EditUserForm from './editUserForm';
+
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 function ProfilePage(): JSX.Element {
   const user = useSelector((state: RootState) => state.auth.user);
@@ -21,11 +22,11 @@ function ProfilePage(): JSX.Element {
 
   const openEditModal = (): void => {
     setEditModal(true);
-  }
+  };
 
   const closeModalEdit = (): void => {
     setEditModal(false);
-  }
+  };
 
   const closeModal = (): void => {
     setViewModal(false);
@@ -41,7 +42,7 @@ function ProfilePage(): JSX.Element {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 1,
+    slidesToShow: 4,
     slidesToScroll: 1,
   };
 
@@ -64,7 +65,7 @@ function ProfilePage(): JSX.Element {
       display: 'flex',
       flexDirection: 'column',
       backgroundColor: '#242424',
-      width: '30%',
+      width: '80%',
     },
   };
 
@@ -84,18 +85,51 @@ function ProfilePage(): JSX.Element {
           marginBottom: '20px',
         }}
       >
-        <img style={{ width: '200px', borderRadius: '50%', overflow: 'hidden' }} src={user?.avatar} alt={user?.name} />
+        <img
+          style={{ width: '200px', borderRadius: '50%', overflow: 'hidden' }}
+          src={user?.avatar}
+          alt={user?.name}
+        />
         <h3>Имя пользователя: {user?.name}</h3>
-        <button onClick={openEditModal} type="button">Редактировать</button>
+        <h3>Email: {user?.email}</h3>
+        <button onClick={openEditModal} type="button">
+          Редактировать
+        </button>
       </div>
-      <div style={{ width: '300px', marginLeft: '20px' }}>
+      <div
+        className="albom1"
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          marginRight: '280px',
+          boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
+          height: '80px', // Вы можете настроить высоту в соответствии с вашими потребностями
+        }}
+      >
+        <p>Альбом</p>
+      </div>
+      <div
+        className="slider-container"
+        style={{
+          width: '80%',
+          marginLeft: '20px',
+          border: '2px solid black',
+          borderRadius: '10px',
+          boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)'
+        }}
+      >
         <Slider {...sliderSettings}>
           {user?.PhotoAlbums?.map((photo) => (
             <div
               key={photo.id}
-              style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+              className="imgPage flex flex-col items-center justify-center w-300" // Добавлен класс w-300 для установки ширины 300px
             >
-              <img style={{ width: '200px' }} src={photo.img} alt={photo.name} />
+              <img
+                style={{ width: '200px', marginBottom: '10px' }}
+                src={photo.img}
+                alt={photo.name}
+              />
               <p>{photo.name}</p>
 
               <img
@@ -107,10 +141,24 @@ function ProfilePage(): JSX.Element {
             </div>
           ))}
         </Slider>
+        <button
+  type="button"
+  style={{
+    marginTop: '30px',
+    padding: '10px 20px', // Увеличиваем отступы для более выразительного вида
+    fontSize: '16px', // Увеличиваем размер шрифта
+    backgroundColor: 'black', // Цвет фона кнопки
+    color: 'white', // Цвет текста кнопки
+    border: 'none',
+    borderRadius: '5px',
+    cursor: 'pointer',
+    boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)', // Добавляем тень
+  }}
+  onClick={openModal}
+>
+  Добавить
+</button>
       </div>
-      <button type="button" style={{ marginTop: '30px' }} onClick={openModal}>
-        Добавить
-      </button>
 
       <Modal
         isOpen={viewModal}
@@ -121,7 +169,12 @@ function ProfilePage(): JSX.Element {
         <AddPhoto closeModal={closeModal} />
       </Modal>
 
-      <Modal isOpen={editModal} onRequestClose={closeModalEdit} style={customStyles} contentLabel="Example Modal">
+      <Modal
+        isOpen={editModal}
+        onRequestClose={closeModalEdit}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
         <EditUserForm closeModalEdit={closeModalEdit} user={user} />
       </Modal>
     </>
